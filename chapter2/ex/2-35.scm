9481fef450ef4d10835ef0e6e9ffe0260f33fe04
Knium <hi.im.knium@gmail.com>
2018-04-24 20:58:48 +0900

    map is ridulous, however powerful

--- chapter2/ex/2-35.scm
+++ chapter2/ex/2-35.scm
@@ -5,7 +5,7 @@
 
 (use slib)
 (require 'trace)
-(trace accumulate)
+; (trace accumulate)
 
 (define (enumerate-tree tree)
   (cond ((null? tree) (list))
@@ -17,7 +17,22 @@
   (accumulate (lambda (x y) (+ x y)) 0 (map (lambda (x) (length (enumerate-tree x))) tree)))
 
 (define tester (list 1 2 (list 3 4) (list 5 6 (list 7 8) 9) 10))
-(print tester)
-(print (map (lambda (x) (length (enumerate-tree x))) tester))
-(print (enumerate-tree tester))
+; (print tester)
+; (print (map (lambda (x) (length (enumerate-tree x))) tester))
+; (print (enumerate-tree tester))
+; (print (count-leaves tester))
+
+(define (count-leaves tree)
+  (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1)) tree)))
+
+(trace count-leaves)
 (print (count-leaves tester))
+; CALL count-leaves (1 2 (3 4) (5 6 (...) 9) 10)
+;   CALL count-leaves (3 4)
+;   RETN count-leaves 2
+;   CALL count-leaves (5 6 (7 8) 9)
+;     CALL count-leaves (7 8)
+;     RETN count-leaves 2
+;   RETN count-leaves 5
+; RETN count-leaves 10
+; 10
