8a1345ef30240febb7c4bff1d28ced77b4089382
Knium <hi.im.knium@gmail.com>
2018-04-26 00:41:02 +0900

    add: library source code
    add: trace

---
+++ lib.scm
@@ -0,0 +1,33 @@
+(use slib)
+(require 'trace)
+
+(define (accumulate op init seq)
+  (if (null? seq) init
+    (op (car seq)
+        (accumulate op init (cdr seq)))))
+
+(define (prime? n)
+
+  (define (smallest-divisor n)
+    (find-divisor n 2))
+
+  (define (divides? n d)
+    (= (modulo n d) 0))
+
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? n test-divisor) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+  (= n (smallest-divisor n)))
+
+(define (enumerate-trees tree)
+  (cond ((null? tree) (list)) 
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-trees (car tree))
+                      (enumerate-trees (cdr tree))))))
+
+(define (enumerate-intervals low high)
+  (if (< high low) (list)
+    (cons low (enumerate-intervals (+ 1 low) high))))
+
