359d828aced807a48e2cfb274b0b789ed7162258
Knium <hi.im.knium@gmail.com>
2018-04-26 12:58:12 +0900

    done

---
+++ chapter2/ex/2-53.scm
@@ -0,0 +1,14 @@
+(list 'a 'b 'c) ; (a b c)
+(list (list 'george)) ; ((george))
+(cdr '((x1 x2) (y1 y2))) ; ((y1 y2))
+(cadr '((x1 x2) (y1 y2))) ; (y1 y2)
+(pair? (car '(a short list))) ; #f
+
+(define (memq item x)
+  (cond ((null? x) #f)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+
+(memq 'red '((red shoes) (blue socks))) ; #f
+(memq 'red '(red shoes blue socks)) ; #t -> (red shoes blue socks)
+
