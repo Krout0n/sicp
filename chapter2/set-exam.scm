3f5cce27379ff3833ef442924842c759812e62c5
Knium <hi.im.knium@gmail.com>
2018-04-28 15:27:23 +0900

    learn it seriously

--- chapter2/set-exam.scm
+++ chapter2/set-exam.scm
@@ -1,9 +1,26 @@
+; 集合を表現する方法として重複なしのリストを構成する方法がある．
+; element-of-set? はmemqと似たような実装になる.
+
+(load "../lib.scm")
+
 (define (element-of-set? x set)
-  (cond ((null? x) #f)
+  (cond ((null? set) #f)
         ((equal? x (car set)) #t)
-        (else (element-of-set x (cdr x)))))
+        (else (element-of-set? x (cdr set)))))
+
+(element-of-set? 1 (list 5 6 1 8 2))
 
 (define (adjoin-set x set)
   (if (element-of-set? x set) set
     (cons x set)))
+
+(adjoin-set 1 (list 2 3 4 5))
+
+(define (intersection-set s1 s2) ; 積集合
+  (cond ((or (null? s1) (null? s2)) (list))
+        ((element-of-set? (car s1) s2) (cons (car s1) (intersection-set (cdr s1) s2)))
+        (else (intersection-set (cdr s1) (cdr s2))))) 
+
+(trace intersection-set)
+(intersection-set (list 2 3 5 7) (list 2 4 6 8))
 
