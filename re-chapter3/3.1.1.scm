44bddce94a00a5513c3bf271426bc2d261a0bc26
Krouton <hi.im.knium@gmail.com>
2020-03-28 00:27:21 +0900

    まだだめです

--- re-chapter3/3.1.1.scm
+++ re-chapter3/3.1.1.scm
@@ -1,10 +1,15 @@
 ; これじゃ口座みんな一緒だしwithdrawがグローバルに依存していて気持ち悪い
-(define balance 100)
-(define (withdraw amount)
-    (if (>= balance amount)
-        (begin (set! balance (- balance amount))
-            balance)
-        "Insufficient funds"))
 
-(withdraw 25)
-(withdraw 25)
+(define new-withdraw
+    (let ((balance 100))
+        (lambda (amount)
+            (if (>= balance amount)
+                (begin
+                    (set! balance (- balance amount))
+                    balance)
+                "Insufficient funds"))))
+
+(define mine new-withdraw)
+(define yours new-withdraw)
+(mine 25) ; 75
+(yours 10) ; 65
