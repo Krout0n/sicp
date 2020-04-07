725d41a9d1d74df73cb44dd12a783fa488ea6b62
Krout0n <krouton@hatena.ne.jp>
2020-04-07 18:04:33 +0900

    WIP

--- chapter4/meta.scm
+++ chapter4/meta.scm
@@ -247,4 +247,47 @@
                 ((eq? var (car vars)) (set-car! vals val)) ; 上書き
                 (else (scan (cdr vars) (cdr vals)))))))
 
+(define (setup-environment)
+    (let
+        ((initial-env (extend-environment
+                        (primitive-procedure-names)
+                        (primitive-procedure-objects)
+                        the-empty-environment)))
+        (define-variable! 'true true initial-env)
+        (define-variable! 'false false initial-env))
+        initial-env)
 
+(define the-global-environment (setup-environment))
+(define (primitive-procedure? proc) (tagged-list? 'primitive))
+(define (primitive-implementation proc) (cadr proc))
+(define (primitive-procedures)
+    (list
+        (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        ; <more primitives>))
+
+(define (primitive-procedure-names)
+    (map car primitive-procedures))
+(define (primitive-procedure-objecs)
+    (map
+        (lambda (proc) (list 'primitive (cadr proc)))
+        primitive-procedures))
+
+(define (apply-primitive-procedure proc args)
+    (apply-in-underlying-scheme
+        (primitive-implementation proc)
+        args))
+
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+(define (driver-loop)
+    (prompt-for-input input-prompt)
+    (let
+        ((input (read)))
+        (let
+            ((output (eval input the-global-environment)))
+            (announce-output output-prompt)
+            (user-print output)))
+    (driver-loop))
